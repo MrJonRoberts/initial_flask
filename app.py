0e8825bb6fcc26b12e4b4719958ef6d2dc9927b4
Jon Roberts <jroberts@tas.qld.edu.au>
2020-03-12 12:06:14 +1000

    git is messy with firewalls - fixed now!

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 
 @app.route('/')
@@ -12,20 +13,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run()
-=======
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    title = "Home Page"
-    pageHeader = "This is the home page"
-    return render_template("home.html", title=title, pageHeader=pageHeader)
-
-
-if __name__ == '__main__':
-    app.run()
->>>>>>> 0e50d8fc17dfe4592d59d27939703059391608a1
+    app.run(debug=True)
