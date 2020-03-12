582de0f0d38f7b81a2b3cb843739dde1094605ea
Jon Roberts <jroberts@tas.qld.edu.au>
2020-03-12 10:47:19 +1000

    Initial

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    title = "Home Page"
+    pageHeader = "This is the home page"
+    return render_template("home.html", title=title, pageHeader=pageHeader)
+
+
+if __name__ == '__main__':
+    app.run()
